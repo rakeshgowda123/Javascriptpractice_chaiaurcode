dd984c03d7b46e758a55b6cf72a6d498479fe5c8
Rakesh Gowdra <rakeshgowder251999@gmail.com>
2024-11-22 16:11:13 +0530

    Initial commit

---
+++ number.js
@@ -0,0 +1,18 @@
+let balance = 15235235334.997979;
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+// console.log(balance.toLocaleString(en - IN));
+
+// ++++++++++Maths Library++++++++++++++
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.333));
+// console.log(Math.floor(4.33));
+// console.log(Math.ceil(4.33));
+
+// console.log(Math.random() * 10 + 1);
+
+const min = 5;
+const max = 10;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
